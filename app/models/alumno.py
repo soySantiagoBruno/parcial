79d073bf525d0b5f258a7bc57bca744e3ad5f74f
Santiago Bruno <soysantiagobruno@gmail.com>
2025-08-27 18:00:41 -0300

    armo la clase Alumno para que ande con la base de datos

--- app/models/alumno.py
+++ app/models/alumno.py
@@ -1,14 +1,21 @@
 from datetime import date
+from app import db
 from app.models.tipo_documento import TipoDocumento
+from app.models.facultad import Facultad
 
-class Alumno:
-    def __init__(self, apellido: str, nombre: str, nroDocumento: str, tipoDocumento: TipoDocumento,
-                 fechaNacimiento: str, sexo: str, nroLegajo: int, fechaIngreso: date):
-        self.apellido = apellido
-        self.nombre = nombre
-        self.nroDocumento = nroDocumento
-        self.tipoDocumento = tipoDocumento
-        self.fechaNacimiento = fechaNacimiento
-        self.sexo = sexo
-        self.nroLegajo = nroLegajo
-        self.fechaIngreso = fechaIngreso
+class Alumno(db.Model):
+    __tablename__ = "alumnos"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    apellido = db.Column(db.String(50), nullable=False)
+    nombre = db.Column(db.String(50), nullable=False)
+    nroDocumento = db.Column(db.String(20), nullable=False)
+    tipoDocumento_id = db.Column(db.Integer, db.ForeignKey("tipo_documento.id"), nullable=False)
+    tipoDocumento = db.relationship("TipoDocumento")
+    fechaNacimiento = db.Column(db.Date, nullable=False)
+    sexo = db.Column(db.String(10), nullable=False)
+    nroLegajo = db.Column(db.Integer, unique=True, nullable=False)
+    fechaIngreso = db.Column(db.Date, nullable=False)
+
+    facultad_id = db.Column(db.Integer, db.ForeignKey("facultades.id"))
+    facultad = db.relationship("Facultad")
